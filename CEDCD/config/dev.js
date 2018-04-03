82f9c23f06bec36fb708599a194f325296cec11e
David-YuWei <ywwei85@gmail.com>
2018-04-03 18:14:30 -0400

    Modify port configuration

--- CEDCD/config/dev.js
+++ CEDCD/config/dev.js
@@ -5,5 +5,12 @@
 'use strict';
 
 module.exports = {
-	
+	// Server port
+	port: process.env.PORT || 9221,
+
+	// Server port
+    logDir: process.env.LOGDIR || '/var/log/cedcd',
+
+	//file upload path
+	file_path: process.env.FILE_PATH || "/data/docs/"
 };
